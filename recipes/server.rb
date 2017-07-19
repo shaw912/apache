f2e12d22e6cddf2b5683c0afd4aa1927e9176687
shaw <shashank_g2006@yahoo.com>
2017-07-19 03:21:00 +0000

    added hostname and IPaddress to server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,8 +2,13 @@ package 'httpd' do
   action :'install'
 end
 
+#node['hostname']
+#node['ipaddress']
 file '/var/www/html/index.html' do
-  content '<h1>Hello,World!</h1>'
+  content "<h1>Hello,World!</h1>
+  HOSTNAME:#{node['hostname']}
+  IPADDRESS:#{node['ipaddress']} 
+"
 end
 
 service 'httpd' do
