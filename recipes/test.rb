f9a2f86df9c9b78135c23fd24f609dabcf0918ec
shaw <shashank_g2006@yahoo.com>
2017-07-19 15:29:51 +0000

    added rspec details and changed rspec parameters

---
+++ recipes/test.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: apache
+# Recipe:: test
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -9,10 +9,21 @@ require 'spec_helper'
 describe 'apache::default' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new
+      runner = ChefSpec::ServerRunner.new({:platform => 'centos', :version => '6.7'})
       runner.converge(described_recipe)
     end
-
+    it 'installs the correct package' do
+      expect(chef_run).to install_package('httpd')
+    end
+    it 'creates a default html file' do
+      expect(chef_run).to create_template('/var/www/html/index.html')
+    end
+    it 'starts the service' do
+      expect(chef_run).to start_service('httpd')
+    end
+    it 'enables the service' do
+      expect(chef_run).to enable_service('httpd')
+    end
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
