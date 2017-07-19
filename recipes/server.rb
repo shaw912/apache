f66a89db589c1dd17e3f3641b8aa4937749cf61e
shaw <shashank_g2006@yahoo.com>
2017-07-19 15:09:19 +0000

    added test kitchen configuration

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,20 +2,58 @@ package 'httpd' do
   action :'install'
 end
 
-#node['hostname']
-#node['ipaddress']
-#file '/var/www/html/index.html' do
-#  content "<h1>Hello,World!</h1>
-#  HOSTNAME:#{node['hostname']}
-#  IPADDRESS:#{node['ipaddress']} 
-#"
+# notifies :action, 'resource[name]', :timer
+# subscribe :action, 'resource[name]', :esesource
+# :before, :delayed, :immediately
+#
+
+
+#
+#cookbook_file '/var/www/html/index.html' do
+#  source 'index.html'
 #end
+
+remote_file 'var/www/html/shaw.jpg' do
+  source 'https://s3-ap-southeast-2.amazonaws.com/shaw912/shaw1.jpg'
+end
+
 
 template '/var/www/html/index.html' do
   source 'index.html.erb'
+ # notifies :restart, 'service[httpd]', :immediately
   action :create 
 end
+
+#bash "inline.script" do
+#  user "root"
+#  code "mkdir -p /var/www/mysites/ && chown -R /var/www/mysites/"
+#  not_if '[-d /var/www/mysites/ ]'
+#  not_if do 
+#    File.directory?('/var/www/mysites/')
+#  end
+#end
   
+#execute "run a script" do
+#  user "root"
+#  command <<-EOH
+#  mkdir -p /var/www/mysite/ /
+#  chown -R apache /var/www/mysites/
+#  EOH
+#  not_if
+#end
+
+#execute "run a script" do
+#  user "root"
+#  command './myscript.sh'
+#  not_if
+#end
+
+directory "/var/www/mysites" do
+  owner 'apache'
+  recursive true
+end
+
 service 'httpd' do
   action [:'enable',:'start']
+  subscribes :restart, 'template[/var/www/html/index.html]', :immediately
 end
--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -5,14 +5,18 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at https://docs.chef.io/inspec_reference.html
 
-unless os.windows?
-  describe user('root') do
-    it { should exist }
-    skip 'This is an example test, replace with your own test.'
-  end
-end
+#unless os.windows?
+#  describe user('root') do
+#    it { should exist }
+#    skip 'This is an example test, replace with your own test.'
+#  end
+#end
 
 describe port(80) do
-  it { should_not be_listening }
-  skip 'This is an example test, replace with your own test.'
+  it { should be_listening }
+  its ('protocols') {should include 'tcp6'}
+end
+
+describe command('curl localhost')do
+  its(:stdout) {should match(/Hello,NEW World!/)}
 end
