a0246485a22f3e5acfcf9dc5d51dae57b5b5a276
shaw <shashank_g2006@yahoo.com>
2017-07-19 04:32:51 +0000

    refactored the server.rb with the templates index.html.erb

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,13 +4,18 @@ end
 
 #node['hostname']
 #node['ipaddress']
-file '/var/www/html/index.html' do
-  content "<h1>Hello,World!</h1>
-  HOSTNAME:#{node['hostname']}
-  IPADDRESS:#{node['ipaddress']} 
-"
-end
+#file '/var/www/html/index.html' do
+#  content "<h1>Hello,World!</h1>
+#  HOSTNAME:#{node['hostname']}
+#  IPADDRESS:#{node['ipaddress']} 
+#"
+#end
 
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create 
+end
+  
 service 'httpd' do
   action [:'enable',:'start']
 end
